74a9a71c6d3b1f20a816784091c9b7ea7ea4f6f0
nwmtamilplay <E18CA021@GMAIL.COM>
2024-08-14 16:33:51 +0530

    Update index.php

--- moviza-php/index.php
+++ moviza-php/index.php
@@ -1,1 +1,13 @@
-<?php echo "Php WEBSITE";
+<?php
+// api.php
+
+$data = [
+    "name" => "John Doe",
+    "age" => 28,
+    "email" => "johndoe@example.com",
+    "hobbies" => ["reading", "coding", "hiking"]
+];
+
+// Return the data as JSON
+header('Content-Type: application/json');
+echo json_encode($data);
